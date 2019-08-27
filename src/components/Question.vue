ee1817a078e58c1fbba83bda0520b22a71d18377
Leonardo Silverio <leonardo.silverio@brf-br.com>
2019-08-27 15:38:11 -0300

    Added question hover and updated dependencies

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -29,48 +29,53 @@ export default {
 </script>
 
 <style>
-    .question {
-        min-height: 400px;
-        color: #000;
-        background-color: #FFF;
-        width: 70%;
-        border-radius: 20px;
-        font-size: 2.5rem;
+.question {
+	min-height: 400px;
+	color: #000;
+	background-color: #FFF;
+	width: 70%;
+	border-radius: 20px;
+	font-size: 2.5rem;
 
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-    }
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+}
 
-    ul.answers {
-        font-size: 2rem;
-        padding: 0;
-        display: flex;
-        justify-content: space-around;
-        flex-wrap: wrap;
-    }
+ul.answers {
+	font-size: 2rem;
+	padding: 0;
+	display: flex;
+	justify-content: space-around;
+	flex-wrap: wrap;
+}
 
-    .answers li {
-        margin: 20px;
-        border:#1e9c31 2px solid;
-        background: radial-gradient( #1e9c31, #89c454);        
-        border-radius: 8px;
-        width: 40%;
-        color: #FFF;
-        display: flex;
-    }
+.answers li {
+	margin: 20px;
+	border:#1e9c31 2px solid;
+	background: radial-gradient( #1e9c31, #89c454);        
+	border-radius: 8px;
+	width: 40%;
+	color: #FFF;
+	display: flex;
+}
 
-    .answers .number {
-        padding: 10px;
-        background: #1e9c31;        
-        color: #FFF;
-        flex-basis: 30px;
-        border-top-left-radius: 8px;
-        border-bottom-left-radius: 8px;
-    }
+.answers .number {
+	padding: 10px;
+	background: #1e9c31;        
+	color: #FFF;
+	flex-basis: 30px;
+	border-top-left-radius: 8px;
+	border-bottom-left-radius: 8px;
+}
 
-    .answers .text {
-        flex: 1;
-        align-self: center;
-    }
+.answers .text {
+	flex: 1;
+	align-self: center;
+}
+
+li:hover {
+	cursor: pointer;
+}
+
 </style>
