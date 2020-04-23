abd03697ffc95739e532bdd4a598f014052f5285
Leonardo Franco <leofranco1305@gmail.com>
2020-04-22 21:02:59 -0300

    Added new questions and updated dependencies

--- src/util/questions.js
+++ src/util/questions.js
@@ -1,65 +1,96 @@
 export default [
-    {
-        text: 'Qual é o coletivo de cães?',
-        answers: [
-            { text: 'MATILHA', correct: true },
-            { text: 'REBANHO', correct: false },
-            { text: 'CARDUME', correct: false },
-            { text: 'MANADA', correct: false }
-        ]
-    },
-    {
-        text: 'Qual é a maior floresta do planeta?',
-        answers: [
-            { text: 'NEGRA', correct: false },
-            { text: 'DE SHERWOOD', correct: false },
-            { text: 'DA TIJUCA', correct: false },
-            { text: 'AMAZÔNICA', correct: true }
-        ]
-    },
-    {
-        text: 'Quem é o primeiro substituto do Presidente?',
-        answers: [
-            { text: 'VEREADOR', correct: false },
-            { text: 'VICE-PRESIDENTE', correct: true },
-            { text: 'SENADOR', correct: false },
-            { text: 'DEPUTADO', correct: false }
-        ]
-    },
-    {
-        text: 'Segundo o romance, que animal era “Moby Dick”?',
-        answers: [
-            { text: 'TUBARÃO', correct: false },
-            { text: 'GOLFINHO', correct: false },
-            { text: 'POLVO', correct: false },
-            { text: 'BALEIA', correct: true }
-        ]
-    },
-    {
-        text: 'Que animal é o Pateta?',
-        answers: [
-            { text: 'CAVALO', correct: false },
-            { text: 'BURRO', correct: false },
-            { text: 'CACHORRO', correct: true },
-            { text: 'RAPOSA', correct: false }
-        ]
-    },
-    {
-        text: 'Qual destes personagens não veste uma capa?',
-        answers: [
-            { text: 'SUPER-HOMEM', correct: false },
-            { text: 'BATMAN', correct: false },
-            { text: 'WOLVERINE', correct: true },
-            { text: 'ZORRO', correct: false }
-        ]
-    },
-    {
-        text: "In The Legend Of Zelda Ocarina Of Time, what is the song used to call Link's horse?",
-        answers: [
-            { text: 'Song of Storms', correct: false },
-            { text: "Zelda's Lullaby", correct: false },
-            { text: "Epona's song", correct: true },
-            { text: "Saria's Song", correct: false }
-        ]
-    },
+  {
+    text: 'What is the past continuous form of "Fall"?',
+    answers: [
+      { text: "falling", correct: true },
+      { text: "fallen", correct: false },
+      { text: "fell", correct: false },
+      { text: "fall", correct: false }
+    ]
+  },
+  {
+    text: "In wich year did the Berlin wall fell?",
+    answers: [
+      { text: "19/12/1986", correct: false },
+      { text: "01/04/1998", correct: false },
+      { text: "29/10/1981", correct: false },
+      { text: "9/11/1989", correct: true }
+    ]
+  },
+  {
+    text: "Who, among the following, isn't a Witcher 3 character?",
+    answers: [
+      { text: "Dandelion", correct: false },
+      { text: "Radovid", correct: true },
+      { text: "Joseph", correct: false },
+      { text: "Lambert", correct: false }
+    ]
+  },
+  {
+    text: "In the book “Animal Farm”, what is the name of the Donkey?",
+    answers: [
+      { text: "Bolsonaro", correct: false },
+      { text: "Roosevelt", correct: false },
+      { text: "Donkey, duh", correct: false },
+      { text: "Benjamin", correct: true }
+    ]
+  },
+  {
+    text: "Wich of the following isn't a subatomical particle?",
+    answers: [
+      { text: "Charm", correct: false },
+      { text: "Strange", correct: false },
+      { text: "Volta", correct: true },
+      { text: "Top", correct: false }
+    ]
+  },
+  {
+    text: "Which of the following movies won at 2020 Oscar's celebration as Special effects?",
+    answers: [
+      { text: "Jojo Rabbit", correct: false },
+      { text: "Parasite", correct: false },
+      { text: "1917", correct: true },
+      { text: "Joker", correct: false }
+    ]
+  },
+  {
+    text:
+      "In The Legend Of Zelda Ocarina Of Time, what is the song used to call Link's horse?",
+    answers: [
+      { text: "Song of Storms", correct: false },
+      { text: "Zelda's Lullaby", correct: false },
+      { text: "Epona's song", correct: true },
+      { text: "Saria's Song", correct: false }
+    ]
+  },
+  {
+    text:
+      "What is the name of the sucessor of Hubble space telescope?",
+    answers: [
+			{ text: "James Webb", correct: true },
+      { text: "WFIRST", correct: false },
+      { text: "Hawking telescope", correct: false },
+      { text: "Soyuz MS-14", correct: false }
+    ]
+  },
+  {
+    text:
+      "What is the main characteristic of a brown-dwarf star?",
+    answers: [
+			{ text: "It's color", correct: false },
+      { text: "It's between a planet and a star", correct: true },
+      { text: "The color is just a form of categorization", correct: false },
+      { text: "The dominant reflection color of the star being brown", correct: false }
+    ]
+  },
+  {
+    text:
+      "Who is the ghost character of Mario game series?",
+    answers: [
+			{ text: "Koopa Troopa", correct: false},
+      { text: "Boo", correct: true },
+      { text: "Toadstool", correct: false },
+      { text: "Goomba", correct: false }
+    ]
+  }
 ]
